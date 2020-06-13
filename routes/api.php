1034e958298511809a53dd2b68c489aff34b783f
Andrey Ignatyev <ignatyev@andrey.com.au>
2020-06-13 18:12:33 +1000

    Api routes rules adjusted, basic routes added

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('Admin')->group(function () {
+    Route::middleware([])->group(function () {
+        Route::get('/generate-stats', 'LogFilesController@processAll');
+        Route::get('/reset-stats', 'LogFilesController@removeAll');
+    });
 });
